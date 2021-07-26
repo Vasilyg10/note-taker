0e1693287d1aad8bd12de4ed80ccf8525975e290
Vasilyg10 <zvgianna@gmail.com>
2021-07-25 19:14:34 -0500

    added uniqid and also fixed routes on Routers.

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const db = require('../../db/db');
+const db = require('../../db/db.json');
 const fs = require('fs');
 const path = require('path');
+const uniqid = require('uniqid');
 
 
-router.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '../../db/db'), (err, result) => {
+router.get('/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '../../db/db.json'), (err, result) => {
         if (err) {
             console.log(err);
         }
@@ -15,14 +16,17 @@ router.get('/api/notes', (req, res) => {
     })
 });
 
-router.post('/api/notes', (req, res) => {
-    notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../../db/db'), 'utf8'));
+router.post('/notes', (req, res) => {
+    notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../../db/db.json'), 'utf8'));
 
     const note = req.body;
 
+    note.id = uniqid();
+    res.json(note)
+
     notes.push(note);
 
-    fs.writeFile(path.join(__dirname, '../../db/db'), JSON.stringify(notes), (err, result) => {
+    fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(notes), (err, result) => {
         if (err) {
             console.log(err);
         }
